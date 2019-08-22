bc8ad558561b76d4b19e7480af8ab28d5bf95381
孙小涛 <sunxiaotao@admaster.com.cn>
2019-08-22 14:49:58 +0800

    所有请求方法的单元测试

--- initRouter/initRouter.go
+++ initRouter/initRouter.go
@@ -14,9 +14,33 @@ func SetupRouter() *gin.Engine {
 	// 注册路由
 	// gin.Context 集合了 request, Params 等的属性和方法
 	r.GET("/", func(c *gin.Context) {
-
 		// 原理上，利用 w.writer 生成 json 返回结果
 		c.String(http.StatusOK, "hello gin")
+	})
+
+	// post
+	r.POST("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "hello gin post method")
+	})
+	// put
+	r.PUT("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "hello gin put method")
+	})
+	// delete
+	r.DELETE("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "hello gin delete method")
+	})
+	// post
+	r.PATCH("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "hello gin patch method")
+	})
+	// Head
+	r.HEAD("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "hello gin head method")
+	})
+	// Options
+	r.OPTIONS("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "hello gin options method")
 	})
 
 	return r
